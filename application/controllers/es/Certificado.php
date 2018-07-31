a48058b55c2f82e25869793caff26308752a7166
joseminaya15 <jose.minayac15@gmail.com>
2018-07-31 17:01:45 -0500

    imagenes

--- application/controllers/es/Certificado.php
+++ application/controllers/es/Certificado.php
@@ -43,20 +43,21 @@ class Certificado extends CI_Controller {
 							.js-information h2{
 								font-size: 80px;
 								line-height: 80px;
-								margin: 5px 0;
+								margin: 30px 0 5px 0;
 								font-family: "Roboto",sans-serif;
 								font-weight: bold;
 								color: #000000;
 							}
 							.js-information p{
-								font-size: 20px;
+								font-size: 18px;
+								line-height: 18px;
 								font-family: "Roboto",sans-serif;
 								font-weight: light;
 								color: #231F20;
 							}
 							.js-information h3{
-								font-size: 36px;
-								line-height: 38px;
+								font-size: 32px;
+								line-height: 32px;
 								font-family: "Roboto",sans-serif;
 								font-weight: bold;
 								color: #939598;
@@ -68,36 +69,40 @@ class Certificado extends CI_Controller {
 								margin: 10px 0;
 							}
 							.js-information--curso p{
-								font-size: 20px;
-								line-height: 22px;
+								font-size: 16px;
+								line-height: 16px;
 								margin: 5px 0;
 							}
 							.js-information span{
 								font-size: 14px;
 								font-family: "Roboto",sans-serif;
-								font-weight: light;
+								font-weight: bold;
 								display: block;
 								color: #231F20;
 							}
 						</style>
 					</head>
 					<body>
+						<img width="100%" height="18" src="http://www.sap-latam.com/SAP_Marketing_Academy/public/img/logo/fondo1.png"/>
 						<div class="js-information">
+							<img style="float: right;width: 170px;" src="http://www.sap-latam.com/SAP_Marketing_Academy/public/img/logo/logo-sap--black.png"/>
 							<h2>Certificado</h2>
 							<h3>SAP Marketing Academy</h3>
 							<p>SAP hace constar que:</p>
-							<h3>'.$nombre.' '.$Apellidos.'</h3>
+							<h3 style="border-bottom: 1px solid #000000;padding: 10px 0; margin-bottom: 20px;width:500px;">'.$nombre.' '.$Apellidos.'</h3>
 							<div class="js-information--curso">
 								<p>Ha participado de</p>
 								<h3>Marketing Mix Modeling</h3>
 								<p>Julio del 2018</p>
 							</div>
-							<div class="js-information--firma">
-								<img width="250" style="border-bottom: 1px solid #757575;" src="http://www.sap-latam.com/SAP_Marketing_Academy/public/img/logo/firma.png"/><br><br>
+							<div class="js-information--firma" style="margin-top: 20px;">
+								<img width="200" style="border-bottom: 1px solid #757575;" src="http://www.sap-latam.com/SAP_Marketing_Academy/public/img/logo/firma.png"/><br><br>
 								<span>SAP Partner & SME</span><br>
 								<span>Marketing Latin America</span><br>
 							</div>
+							<img style="float: right;position: absolute;margin-top: -75px;margin-bottom: 14px;" src="http://www.sap-latam.com/SAP_Marketing_Academy/public/img/logo/color1.png"/>
 						</div>
+						<img width="100%" height="18" src="http://www.sap-latam.com/SAP_Marketing_Academy/public/img/logo/fondo1.png"/>
 					</body>
 				  </html>';
 		$mpdf = new \Mpdf\Mpdf(['orientation' => 'L']);
